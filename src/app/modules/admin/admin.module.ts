963aa8545e9d5d9d5cec8bf26ed2166bcdd8de4b
novelkhan <novel4004@gmail.com>
2024-08-20 07:43:09 +0600

    add-edit-member component is created inside admin module in it's dedicated components folder using 'ng g c add-edit-member --skip-tests' command

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './components/admin/admin.component';
+import { AddEditMemberComponent } from './components/add-edit-member/add-edit-member.component';
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    AddEditMemberComponent
   ],
   imports: [
     CommonModule,
