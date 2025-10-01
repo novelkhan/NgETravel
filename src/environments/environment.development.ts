a4f49bec20586650646784aa4bafdba087ee1ee3
novelkhan <novel4004@gmail.com>
2025-10-02 01:32:35 +0600

    HTML tag value is updated

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   apiUrl: 'https://localhost:7039',
   userKey: 'eTravelAppUser',
-  idleTimeoutInMilliSeconds: 10 * 1000, // ১০ মিনিট
+  idleTimeoutInMilliSeconds: 10 * 60 * 1000, // ১০ মিনিট
   countdownDurationInSeconds: 20 // ২০ সেকেন্ড
 };
